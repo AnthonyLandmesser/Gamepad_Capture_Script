1467ec4a450b8da6f31a902f6630e7075c21bae0
AnthonyLandmesser <landmesseranthony@gmail.com>
2025-09-18 16:47:47 -0400

    Added support for pcap dumps for faster testing

--- stream.py
+++ stream.py
@@ -1,3 +1,4 @@
+import sys
 import subprocess
 import av
 import cv2
@@ -9,22 +10,23 @@ UDP_PORT = 50020
 TSHARK_PATH = '/run/current-system/sw/bin/tshark'
 
 # Put wireless adapter in monitor mode
-subprocess.run(['sudo', 'airmon-ng', 'check', 'kill'], check=True)
-subprocess.run(['sudo', 'airmon-ng', 'start', f'{INTERFACE}', f'{CHANNEL}'], check=True)
-subprocess.run(['sudo', 'iw', 'dev', f'{INTERFACE}', 'set', 'monitor', 'fcsfail'], check=True)
+def setup_monitor_mode():
+    subprocess.run(['sudo', 'airmon-ng', 'check', 'kill'], check=True)
+    subprocess.run(['sudo', 'airmon-ng', 'start', f'{INTERFACE}', f'{CHANNEL}'], check=True)
+    subprocess.run(['sudo', 'iw', 'dev', f'{INTERFACE}', 'set', 'monitor', 'fcsfail'], check=True)
 
 # Setup H.264 decoder
-FRAME_START = bytes([0x00, 0x00, 0x00, 0x01])
-SPS_HEADER = bytes([0x67, 0x64, 0x00, 0x20, 0xac, 0x2b, 0x40, 0x6c, 0x1e, 0xf3, 0x68])
-PPS_HEADER = bytes([0x68, 0xEE, 0x06, 0x0C, 0xE8])
-I_SLICE_HEADER = bytes([0x25, 0xb8, 0x04, 0xff])
-P_SLICE_HEADER = bytes([0x21, 0xe0, 0x03, 0xff])
+def setup_codec():
+    global FRAME_START, I_SLICE_HEADER, P_SLICE_HEADER, CODEC
 
-codec = av.CodecContext.create('h264', 'r')
-parameter_sets = codec.parse(FRAME_START + SPS_HEADER + FRAME_START + PPS_HEADER)
+    FRAME_START = bytes([0x00, 0x00, 0x00, 0x01])
+    SPS_HEADER = bytes([0x67, 0x64, 0x00, 0x20, 0xac, 0x2b, 0x40, 0x6c, 0x1e, 0xf3, 0x68])
+    PPS_HEADER = bytes([0x68, 0xEE, 0x06, 0x0C, 0xE8])
+    I_SLICE_HEADER = bytes([0x25, 0xb8, 0x04, 0xff])
+    P_SLICE_HEADER = bytes([0x21, 0xe0, 0x03, 0xff])
 
-# Capture UDP traffic
-capture_command = ['tshark', '-i', f'{INTERFACE}', '-o', 'wlan.enable_decryption:TRUE', '-Y', f'udp.port=={UDP_PORT}', '-T', 'fields', '-e', 'data', '-l']
+    CODEC = av.CodecContext.create('h264', 'r')
+    parameter_sets = CODEC.parse(FRAME_START + SPS_HEADER + FRAME_START + PPS_HEADER)
 
 # Helper functions
 def is_i_frame(payload):
@@ -37,15 +39,11 @@ def is_frame_end(payload):
     return bool(payload[2] >> 4 & 1)
 
 def get_headers(payload, frame_num):
-    parsed_frames = []
-    if is_frame_start(payload):
-        if is_i_frame(payload):
-            parsed_frames = codec.parse(FRAME_START + I_SLICE_HEADER)
-            print("I frame sent")
-        else:
-            curr_header = bytes([P_SLICE_HEADER[0], P_SLICE_HEADER[1] | (frame_num >> 3), P_SLICE_HEADER[2], P_SLICE_HEADER[3] | (frame_num << 5) & 0xff])
-            parsed_frames = codec.parse(FRAME_START + curr_header)
-    return parsed_frames
+    if is_i_frame(payload):
+        print("I frame sent")
+        return FRAME_START + I_SLICE_HEADER
+    else:
+        return FRAME_START + bytes([P_SLICE_HEADER[0], P_SLICE_HEADER[1] | (frame_num >> 3), P_SLICE_HEADER[2], P_SLICE_HEADER[3] | (frame_num << 5) & 0xff])
 
 def get_safe_payload(payload):
     safe_payload = payload[16:18]
@@ -58,7 +56,7 @@ def get_safe_payload(payload):
 
 def show_image(parsed_frames):
     for parsed_frame in parsed_frames:
-        decoded_frames = codec.decode(parsed_frame)
+        decoded_frames = CODEC.decode(parsed_frame)
         for decoded_frame in decoded_frames:
             image = decoded_frame.to_ndarray(format='bgr24')
             #print(image)
@@ -68,7 +66,15 @@ def show_image(parsed_frames):
     return 0
 
 # main
-def main():
+def stream(pcap_file):
+    if pcap_file:
+        capture_command = ['tshark', '-r', f'{pcap_file}', '-o', 'wlan.enable_decryption:TRUE', '-Y', f'udp.port=={UDP_PORT}', '-T', 'fields', '-e', 'data', '-l']
+    else:
+        setup_monitor_mode()
+        capture_command = ['tshark', '-i', f'{INTERFACE}', '-o', 'wlan.enable_decryption:TRUE', '-Y', f'udp.port=={UDP_PORT}', '-T', 'fields', '-e', 'data', '-l']
+
+    setup_codec()
+
     with subprocess.Popen(capture_command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True, bufsize=1) as proc:
         frame_num = 0
         go = False
@@ -79,9 +85,12 @@ def main():
             if not (is_i_frame(payload) or go):
                 continue
 
-            parsed_frames = get_headers(payload, frame_num)
+            if is_frame_start(payload):
+                parsed_frames = CODEC.parse(get_headers(payload, frame_num))
+            else:
+                parsed_frames = []
 
-            parsed_frames += codec.parse(get_safe_payload(payload))
+            parsed_frames += CODEC.parse(get_safe_payload(payload))
 
             if (show_image(parsed_frames)):
                 break
@@ -93,4 +102,8 @@ def main():
                 #print(frame_num)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 2:
+        print("Usage: python stream.py <input.pcap>")
+        sys.exit(1)
+    pcap_file = sys.argv[1] if len(sys.argv) == 2 else None
+    stream(pcap_file)
