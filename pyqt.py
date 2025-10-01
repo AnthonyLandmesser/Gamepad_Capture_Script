3e9398a8386a8c236b710b0ec8137ca26829226a
AnthonyLandmesser <landmesseranthony@gmail.com>
2025-10-01 19:11:55 -0400

    basic initial layout

--- pyqt.py
+++ pyqt.py
@@ -1,16 +1,18 @@
 import sys
 import random
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
 
 class VideoStream(QtWidgets.QWidget):
-    def __init__(self, message):
-        super().__init__()
-        self.resize(400, 300)
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.setWindowTitle("Wii U Gamepad Stream")
+        self.resize(854, 480)
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint | QtCore.Qt.Window)
 
-        label = QtWidgets.QLabel(message, self)
+        label = QtWidgets.QLabel("Hello", self)
 
         self.layout = QtWidgets.QVBoxLayout()
-        self.layout.addWidget(self.label)
+        self.layout.addWidget(label)
     
     def set_image(self, image):
         qimage = QtWidgets.QImage(image, 854, 480, QImage.Format.Format_BGR888)
@@ -28,30 +30,24 @@ class MainContainer(QtWidgets.QWidget):
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.button)
 
-        self.button.clicked.connect(self.request_iframe)
+        self.button.clicked.connect(self.start_stream)
+
+    def start_stream(self):
+        self.output_window = VideoStream(self)
+        self.output_window.show()
 
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.label = QtWidgets.QLabel(self)
-        self.label.setScaledContents(True)
-
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.label)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.request_iframe)
-
-    def request_iframe(self):
-        self.output_window = OutputWindow("Hello World")
-        self.output_window.show()
+        self.setWindowTitle("Wii U Gamepad Monitor")
+        container = MainContainer()
+        self.setCentralWidget(container)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
-    widget = MyWidget()
+    widget = Main()
     widget.resize(1000, 1000)
     widget.show()
 
