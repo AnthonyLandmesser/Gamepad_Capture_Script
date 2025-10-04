cc348c19726276a0d809da1c0659956431820916
AnthonyLandmesser <landmesseranthony@gmail.com>
2025-10-03 22:27:46 -0400

    basic stream window working

--- pyqt.py
+++ pyqt.py
@@ -5,32 +5,46 @@ from PySide6 import QtWidgets, QtCore, QtGui
 
 import stream
 
-class VideoWorker(QtCore.QThread):
+class VideoWorker(QtCore.QObject):
     frame_ready = QtCore.Signal(QtGui.QImage)
-    finished = QtCore.Signal()
 
-    def set_image(self, image):
+    def update_frame(self):
+        parsed_frames = []
         for parsed_frame in parsed_frames:
             decoded_frames = CODEC.decode(parsed_frame)
             for decoded_frame in decoded_frames:
                 image = decoded_frame.to_ndarray(format='bgr24')
                 qimage = QtGui.QImage(image, 854, 480, QtGui.QImage.Format.Format_BGR888)
-                self.label.setPixmap(QtGui.QPixmap.fromImage(qimage))
+                self.frame_ready.emit(qimage.copy())
 
 class VideoStream(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Wii U Gamepad Stream")
-        self.resize(854, 480)
+        self.setFixedSize(854, 480)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
 
         self.label = QtWidgets.QLabel("No video source available.", self)
+        self.worker = VideoWorker()
 
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.label)
 
-        stream.stream(video_stream=self)
         self.show()
+        self.drag_pos = None
+
+    def mousePressEvent(self, event):
+        if event.buttons() == QtCore.Qt.MouseButton.LeftButton:
+            self.drag_pos = event.globalPosition().toPoint() - self.frameGeometry().topLeft()
+        else:
+            self.drag_pos = None
+
+    def mouseMoveEvent(self, event):
+        if self.drag_pos:
+            self.move(event.globalPosition().toPoint() - self.drag_pos)
+
+    def mouseReleaseEvent(self, event):
+        self.drag_pos = None
 
 class MainContainer(QtWidgets.QWidget):
     def __init__(self):
@@ -47,7 +61,14 @@ class MainContainer(QtWidgets.QWidget):
         self.button.clicked.connect(self.start_stream)
 
     def start_stream(self):
+        self.thread = QtCore.QThread()
         self.output_window = VideoStream()
+        self.output_window.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.output_window.worker.update_frame)
+        self.output_window.worker.frame_ready.connect(
+            lambda qimage: self.label.setPixmap(QtGui.QPixmap.fromImage(qimage))
+        )
+        self.thread.start()
 
 class Main(QtWidgets.QMainWindow):
     def __init__(self):
@@ -59,6 +80,7 @@ class Main(QtWidgets.QMainWindow):
 
     def closeEvent(self, event):
         if self.container.output_window:
+            self.container.thread.quit()
             self.container.output_window.close()
         super().closeEvent(event)
 
